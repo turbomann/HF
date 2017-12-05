8b5a70f4e02df61c534ef955b0e58e6d37777206
trx0eth7 <bahdepa2@gmailcom>
2017-12-06 00:50:15 +0400

    complete exercise DooBeeDooBeeDo

---
+++ src/main/java/trx0eth7/chapter1/DooBe.java
@@ -0,0 +1,16 @@
+package trx0eth7.chapter1;
+
+public class DooBe {
+    //ER: DooBeeDooBeeDo
+    public static void main(String ... args){
+        int x = 1;
+        while (x<3){
+            System.out.print("Doo");
+            System.out.print("Bee");
+            ++x;
+        }
+        if(x==3){
+            System.out.print("Do");
+        }
+    }
+}
