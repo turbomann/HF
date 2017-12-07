98aa8163ec96fb11350f068df0d61780be6cf47f
Roman <headsimulation@gmail.com>
2017-12-08 00:05:03 +0400

    Guessing game

---
+++ src/main/java/turbomann/Chapter_2/GameLauncher.java
@@ -0,0 +1,11 @@
+package turbomann.Chapter_2;
+
+/**
+ * Created by Roman on 07.12.2017.
+ */
+public class GameLauncher {
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.StartGame();
+    }
+}
