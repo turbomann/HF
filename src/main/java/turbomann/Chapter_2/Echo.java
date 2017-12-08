3acdcba56b887a1308694178fe6379b4a5b0ba35
Roman <headsimulation@gmail.com>
2017-12-08 23:57:12 +0400

    Without pool

---
+++ src/main/java/turbomann/Chapter_2/Echo.java
@@ -0,0 +1,15 @@
+package turbomann.Chapter_2;
+
+/**
+ * Created by Roman on 08.12.2017.
+ */
+class EchoTestDrive {
+    public static void main(String[] args) {
+        Echo e1 = new Echo();
+
+    }
+}
+class Echo {
+
+}
+
