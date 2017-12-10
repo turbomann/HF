9d76d108973d4fece2485cd5cbf707f562800d20
trx0eth7 <bahdepa2@gmailcom>
2017-12-10 16:16:45 +0400

    Added Matcher & Pattern to search for the shape type through regular expression.

--- src/main/java/trx0eth7/chapter2/programm/Main.java
+++ src/main/java/trx0eth7/chapter2/programm/Main.java
@@ -1,30 +1,49 @@
 package trx0eth7.chapter2.programm;
 
 import trx0eth7.chapter2.programm.model.*;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import static trx0eth7.chapter2.programm.builder.BuilderShape.createShape;
 
+/**
+ * Sources:
+ *
+ * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/Scanner.html">Scanner</a>
+ * @see <a href="https://habrahabr.ru/post/260773/">Pattern, Matcher</a>
+ */
 public class Main {
+    private List<Pattern> patterns = new ArrayList<>();
+
     public static void main(String... args) {
         Main main = new Main();
-        List<IShape> shapes = main.init();
-        for(IShape shape : shapes){
+        main.init();
+        IShape shape;
+
+        //Program use console
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Possible options:\nCircle\nSquare\nTriangle\nEnter type shape:");
+        String typeShape = scanner.nextLine();
+
+        if (main.isMatch(typeShape)) {
+            shape = createShape(typeShape);
             shape.rotate();
             shape.playSound();
         }
     }
 
-    private List<IShape> init(){
-        //User set shapes
-        List<IShape> shapes = new ArrayList<>();
-
-        shapes.add(createShape("circle"));
-        shapes.add(createShape("square"));
-        shapes.add(createShape("triangle"));
-
-        return shapes;
+    private void init() {
+        patterns.add(Pattern.compile("(^circle$)", Pattern.CASE_INSENSITIVE));
+        patterns.add(Pattern.compile("(^square$)", Pattern.CASE_INSENSITIVE));
+        patterns.add(Pattern.compile("(^triangle$)", Pattern.CASE_INSENSITIVE));
     }
 
+    private boolean isMatch(String typeShape) {
+        return patterns.parallelStream()
+                .anyMatch(pattern -> pattern.matcher(typeShape).matches());
+    }
 }
